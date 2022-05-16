a8be8fd72f1bd39a3984cadec2948d9499a746ea
Jordan Olivares <j.olivares28@outlook.com>
2022-05-15 21:04:50 -0500

    Added two if statements for typeof and length of number

--- app.js
+++ app.js
@@ -5,19 +5,26 @@
 const months = ['Octber', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'];
 
 const biennialUpdate = dotNum => {
+
+    if(typeof dotNum !== 'number') return 'Numbers only.';
+
     const numArr = Array.from(String(dotNum), Number);
+    if(numArr.length >= 9) return 'Invalid Number';
 
     const lastNum = numArr.pop();
     const secondToLastNum = numArr.pop();
     const month = months[lastNum];
     const oddOrEvenYear = (secondToLastNum % 2) ? 'Odd' : 'Even';
 
-    console.log(month);
-    console.log(secondToLastNum);
-    console.log(oddOrEvenYear);
+    const message = `Your biennial update is due on ${month} every ${oddOrEvenYear} years.`;
+
+
+    return message;
 
 };
 
 
-biennialUpdate(324570);
-biennialUpdate(324529);
+console.log(biennialUpdate(324510));
+console.log(biennialUpdate(324529));
+console.log(biennialUpdate('Not a Number!'));
+console.log(biennialUpdate(98764556876567876));
